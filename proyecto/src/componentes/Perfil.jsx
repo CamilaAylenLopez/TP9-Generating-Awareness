7debfe138f66c27b8ab74add78ca8444140f01e8
Gabriel Guivisdalsky <santiago.kasses@gmail.com>
2023-09-20 09:06:37 -0300

    a

--- proyecto/src/componentes/Perfil.jsx
+++ proyecto/src/componentes/Perfil.jsx
@@ -5,11 +5,15 @@ import { UsuarioContext } from "../context/UsuarioContext";
 
 const Perfil = () => {
     const {usuario, setUsuario} = React.useContext(UsuarioContext);
+    const {reciclable, setReciclable} = React.useContext(UsuarioContext);
+    const {trivia, setTrivia} = React.useContext(UsuarioContext);
 
     return (
         <>
         <h1 className='titulo'>Perfil</h1>
         <h1>Nombre: {usuario}</h1>
+        <h1>Puntos del juego reciclable: {reciclable}</h1>
+        <h1>Puntos del juego trivia: {trivia}</h1>
         <div className='centrarJust'>
             <button className='boton' onClick={() => setUsuario('Kasses')}>Cambiar perfil</button>
         </div>
--- proyecto/src/context/UsuarioContext.jsx
+++ proyecto/src/context/UsuarioContext.jsx
@@ -4,8 +4,10 @@ export const UsuarioContext = createContext();
 
 const UsuarioProvider = (props) => {
     const [usuario, setUsuario] = useState('camila');
+    const [reciclable, setReciclable] = useState(0);
+    const [trivia, setTrivia] = useState(0);
 
-    return <UsuarioContext.Provider value={{usuario, setUsuario}}>{props.children}</UsuarioContext.Provider>
+    return <UsuarioContext.Provider value={{usuario, setUsuario, reciclable, setReciclable, trivia, setTrivia}}>{props.children}</UsuarioContext.Provider>
 }
 
 export default UsuarioProvider;
